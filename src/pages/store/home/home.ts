6a45749ef8748a7656892d7050309a36c4ee01dc
Saul Hillar <saulhillar@outlook.es>
2026-04-28 11:45:12 -0300

    add allitems and medias queries

--- src/pages/store/home/home.ts
+++ src/pages/store/home/home.ts
@@ -1,20 +1,25 @@
 import { getCategories, PRODUCTS } from "../../../data/data.js";
 import type { CartItem, Product } from "../../../types/product";
 
-
+// Seleccionamos los elementos del DOM que vamos a utilizar
 const productsContainer = document.querySelector(".products-container") as HTMLDivElement;
 const logoutButton = document.getElementById("logout-btn") as HTMLButtonElement;
 
+// Si el botón de logout existe, le agregamos un evento para redirigir al login
 logoutButton?.addEventListener("click", () => {
     window.location.href = "/src/pages/auth/login/login.html";
 });
 
+// Función para renderizar los productos en la página
+
 function renderProducts(products: Product[]) {
 
+    // Si no hay contenedor, no hacemos nada
     if (!productsContainer) return;
 
     productsContainer.innerHTML = "";
 
+    // Iteramos sobre los productos y creamos las tarjetas correspondientes
     products.forEach((product: Product) => {
         const productCard = document.createElement("div") as HTMLDivElement;
         productCard.classList.add("product-card");
@@ -39,17 +44,24 @@ function renderProducts(products: Product[]) {
         
         const addToCartButton = document.createElement("button") as HTMLButtonElement;
         addToCartButton.textContent = "Agregar al carrito";
-        addToCartButton.addEventListener("click", () => {
+
+        // Evento para agregar el producto al carrito
+        addToCartButton?.addEventListener("click", () => {
+
+            // Obtenemos el carrito actual del localStorage o inicializamos uno nuevo
             const cartItem: CartItem [] = JSON.parse(localStorage.getItem("cart") || "[]");
 
+            // Verificamos si el producto ya está en el carrito
             const existingItemIndex = cartItem.findIndex((item: CartItem) => item.id === product.id);
 
+            // Si el producto ya está en el carrito, incrementamos la cantidad, de lo contrario, lo agregamos con cantidad 1
             if (existingItemIndex !== -1) {
                 cartItem[existingItemIndex].cantidad ++;
             } else {
                 cartItem.push({ ...product, cantidad: 1 });
             }
 
+            // Guardamos el carrito actualizado en el localStorage
             localStorage.setItem("cart", JSON.stringify(cartItem));
             
             updateCartBadge();
@@ -63,16 +75,18 @@ function renderProducts(products: Product[]) {
             }, 1000);
         });
 
-        productCard.append(productImage, productCategory, productDescription, productName, productPrice, addToCartButton);
-        productsContainer.appendChild(productCard);
+
+        // Agregamos los elementos a la tarjeta y luego la tarjeta al contenedor
+        productCard?.append(productImage, productCategory, productDescription, productName, productPrice, addToCartButton);
+        productsContainer?.appendChild(productCard);
     });
 
 };
 
-
+// Función para manejar la búsqueda de productos
 const searchInput = document.getElementById("searchInput") as HTMLInputElement;
 
-    searchInput.addEventListener("input", () => {
+    searchInput?.addEventListener("input", () => {
         const searchTerm = searchInput.value.toLowerCase();
         const filteredProducts = PRODUCTS.filter((product) =>
             product.nombre.toLowerCase().includes(searchTerm)
@@ -80,7 +94,7 @@ const searchInput = document.getElementById("searchInput") as HTMLInputElement;
         renderProducts(filteredProducts);
     });
 
-
+// Renderizado inicial de productos y categorías
 
 function renderCategories() {
     const categoriasList = document.getElementById("categoriasList") as HTMLUListElement;
@@ -89,13 +103,28 @@ function renderCategories() {
     if (!categoriasList) return;
 
     categoriasList.innerHTML = "";
+
+    // Opción para mostrar todos los productos
+    const allItem = document.createElement("li") as HTMLLIElement;
+        allItem.textContent = "Todas";
+        allItem.classList.add("category-item");
+
+        allItem.addEventListener("click", () => {
+            renderProducts(PRODUCTS);
+        });
+
+        categoriasList?.appendChild(allItem);
+    
+        // Iteramos sobre las categorías y creamos los elementos correspondientes
     categorias.forEach((categoria: { nombre: string; id: number }) => {
+
         const categoryItem = document.createElement("li") as HTMLLIElement;
         categoryItem.textContent = categoria.nombre;
-        categoriasList.classList.add("category-item");
-        categoriasList.appendChild(categoryItem);
+        categoryItem.classList.add("category-item");
+        categoriasList?.appendChild(categoryItem);
         
-        categoryItem.addEventListener("click", () => {
+        // Evento para filtrar los productos por categoría al hacer clic en el nombre de la categoría
+        categoryItem?.addEventListener("click", () => {
             const filteredProducts = PRODUCTS.filter((product: Product) =>
                 product.categorias.some((cat: { id: number }) => cat.id === categoria.id)
             );
@@ -103,6 +132,9 @@ function renderCategories() {
         });
     });
 }
+
+// Función para actualizar el contador del carrito en el header
+
 
 function updateCartBadge() {
     const cartItems: CartItem[] = JSON.parse(localStorage.getItem("cart") || "[]");
@@ -114,6 +146,7 @@ function updateCartBadge() {
     }
 }
 
+// Renderizado inicial
 
 renderProducts(PRODUCTS);
 renderCategories();
